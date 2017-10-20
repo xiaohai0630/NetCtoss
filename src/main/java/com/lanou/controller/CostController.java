350a5ca880d86d33bfa7c563526457c0b8ec7a60
dllo <1084372318@qq.com>
2017-10-20 17:33:44 +0800

    导入前端页面，创建Cost表对应的类、接口mapper等，SSM框架的配置

---
+++ src/main/java/com/lanou/controller/CostController.java
@@ -0,0 +1,17 @@
+package com.lanou.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by dllo on 17/10/20.
+ */
+@Controller
+public class CostController {
+
+    /**
+     *
+     */
+
+    
+
+}
